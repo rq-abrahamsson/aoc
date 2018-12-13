ff4589e9100142d7cb13d3dd79b0abc36d879305
Robin Abrahamsson <robin.abrahamsson25@gmail.com>
2018-12-13 20:32:08 +0100

    Done with 5b

---
+++ 2018-5/app-b.exs
@@ -0,0 +1,67 @@
+defmodule Solve do
+  def toArray(body) do
+    String.split(String.trim(body), "")
+  end
+
+  def removeReactions(x, acc) do
+    if acc != [] do
+      first = hd(acc)
+
+      if Solve.reacts?(first, x) do
+        tl(acc)
+      else
+        [x] ++ acc
+      end
+    else
+      [x]
+    end
+  end
+
+  def getUniqLetters(string) do
+    string
+    |> String.downcase()
+    |> toArray()
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.uniq()
+  end
+
+  def getLetterValue(letter, string) do
+    string
+    |> toArray()
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.filter(fn x -> x != String.upcase(letter) && x != letter end)
+    |> Enum.reduce([], &Solve.removeReactions/2)
+    |> Enum.join("")
+    |> String.length()
+  end
+
+  def solve(body) do
+    result =
+      body
+      |> getUniqLetters()
+      |> Enum.map(fn x ->
+        getLetterValue(x, body)
+      end)
+      |> Enum.min()
+
+    IO.inspect(result)
+  end
+
+  def reacts?(letter1, letter2) do
+    !String.equivalent?(letter1, letter2) &&
+      (String.equivalent?(String.downcase(letter1), letter2) ||
+         String.equivalent?(letter1, String.downcase(letter2)))
+  end
+
+  def run() do
+    case File.read("input.txt") do
+      {:ok, body} ->
+        Solve.solve(body)
+
+      {:error, reason} ->
+        IO.puts(reason)
+    end
+  end
+end
+
+Solve.run()
