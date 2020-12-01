2ddb81ff8c499aee2004f5f51e4c90e057b1135d
Robin Abrahamsson <robin@abrahamsson.tech>
2020-12-01 09:52:24 +0100

    Added reading of input to example

--- 2020-example/App.fsx
+++ 2020-example/App.fsx
@@ -3,3 +3,11 @@ open FSharpPlus
 
 let x = String.replace "old" "new" "Good old days"
 printfn "%A" x
+
+let readLines filePath = System.IO.File.ReadLines(filePath);
+
+let lines = readLines "input.txt"
+printfn "%A" lines
+
+let lineList = lines |> Seq.toList
+printfn "%A" lineList
