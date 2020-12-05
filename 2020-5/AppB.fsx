1a96daf3b08b08f4e6da6f392d80762d819b2805
Robin Abrahamsson <robin@abrahamsson.tech>
2020-12-05 14:05:34 +0100

    Done 2020 day 5.

---
+++ 2020-5/AppB.fsx
@@ -0,0 +1,52 @@
+#r "nuget: FSharpPlus"
+open FSharpPlus
+
+let readLines filePath = System.IO.File.ReadLines(filePath);
+
+let getNextSegment (input : char) ((lower, higher): int * int) =
+    let middle = (higher - lower) / 2
+    if input = 'F' || input = 'L' then
+        (lower, lower + middle)
+    else if input = 'B' || input = 'R' then
+        (lower + middle + 1, higher)
+    else 
+        (lower, higher)
+
+let rec getRow (init: int * int) (chars : char list) =
+    let (lower, higher) = getNextSegment chars.Head init
+    if lower = higher then
+        lower
+    else
+        getRow (lower, higher) chars.Tail 
+
+let getRow128 = getRow (0, 127)
+let getColumn8 = getRow (0, 7)
+
+let getSeatId (input : char List) =
+    let (first, last) = input |> List.splitAt 7
+    let row = getRow128 first
+    let column = getColumn8 last
+    (row * 8) + column 
+
+let getFreeSeat (occupiedSeats : int list) =
+    let possibleNumbers = [0 .. 835]
+    possibleNumbers 
+        |> List.filter (fun seatNumber ->
+            if not (occupiedSeats |> List.exists (fun x -> x = seatNumber)) &&
+                occupiedSeats |> List.exists (fun x -> x = seatNumber - 1 ) &&
+                occupiedSeats |> List.exists (fun x -> x = seatNumber + 1 ) 
+                then
+                true
+            else
+                false
+        )
+
+
+// Run
+readLines "input.txt"
+|> Seq.toList 
+|> List.map Seq.toList
+|> List.map getSeatId
+|> getFreeSeat
+|> List.head
+|> printfn "%A"
