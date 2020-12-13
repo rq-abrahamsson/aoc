726db17abd7bb427415196efcf9e8b8e74007a00
Robin Abrahamsson <robin@abrahamsson.tech>
2020-12-13 11:41:34 +0100

    Naive solution for 13b, next step is to increase with the largest interval

---
+++ 2020-13/AppB.fsx
@@ -0,0 +1,32 @@
+#r "nuget: FSharpPlus"
+open FSharpPlus
+
+let readLines filePath = System.IO.File.ReadLines(filePath);
+
+
+let isAScheduledTime t =
+    fun (``t+``, interval) -> 
+        let time = t + ``t+``
+        time - interval * (time / interval) <> 0L
+
+
+let findT (intervalT0 : int64) (rest : (int64 * int64) list) =
+    Seq.initInfinite (fun x -> (int64 x) * intervalT0)
+    |> Seq.skipWhile (fun t -> rest |> List.exists (isAScheduledTime t))
+    |> Seq.take 1
+    |> Seq.head
+
+let parseBusTimes s =
+    s 
+    |> String.split [","]
+    |> Seq.toList
+    |> List.mapi (fun i x -> (i, x))
+    |> List.filter (fun (_, x) -> x <> "x")
+    |> List.map (fun (x, y) -> (int64 x, int64 y))
+
+readLines "input-test.txt"
+|> Seq.toList
+|> fun [x; y] -> parseBusTimes y
+|> fun x -> printfn "%A" x; x
+|> fun ((_, v)::tail) -> findT v tail
+|> fun x -> printfn "%A" x; x
