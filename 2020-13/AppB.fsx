3fc46abb6ec7a71b8beb10bc8d7b9ecb2415e069
Robin Abrahamsson <robin@abrahamsson.tech>
2020-12-13 16:07:25 +0100

    Closer to a solution for 13b but int32 max exceeded..

--- 2020-13/AppB.fsx
+++ 2020-13/AppB.fsx
@@ -16,6 +16,11 @@ let findT (intervalT0 : int64) (rest : (int64 * int64) list) =
     |> Seq.take 1
     |> Seq.head
 
+let normalizeForFirst (list : (int64 * int64) list) =
+    let (decreaseNumber, _) = list.Head
+    list
+    |> List.map (fun (x, y) -> (x - decreaseNumber, y))
+
 let parseBusTimes s =
     s 
     |> String.split [","]
@@ -24,9 +29,11 @@ let parseBusTimes s =
     |> List.filter (fun (_, x) -> x <> "x")
     |> List.map (fun (x, y) -> (int64 x, int64 y))
 
-readLines "input-test.txt"
+readLines "input.txt"
 |> Seq.toList
 |> fun [x; y] -> parseBusTimes y
+|> List.sortWith (fun (_, x) (_, y) -> int (y - x))
+|> normalizeForFirst
 |> fun x -> printfn "%A" x; x
 |> fun ((_, v)::tail) -> findT v tail
 |> fun x -> printfn "%A" x; x
