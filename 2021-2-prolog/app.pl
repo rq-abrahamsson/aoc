984044037d0b6278009820a76730b39493953669
Robin Abrahamsson <robin@abrahamsson.tech>
2021-12-04 18:00:28 +0100

    Solution for 2021-2b prolog

--- 2021-2-prolog/app.pl
+++ 2021-2-prolog/app.pl
@@ -6,36 +6,62 @@ to_int([Y|Tail],[X| Result]) :-
 
 split(In, Out) :- split_string(In, " ", "", Out).
 
+format_input(Input, FormattedInput) :-
+	split_string(Input, "\n", "", RowList),
+	maplist(split, RowList, FormattedInput).
 
-solve_a([], 0, 0).
-solve_a([["forward", Value]|Tail], SumForward, ODown) :-
+% A
+
+handle_command_a([], 0, 0).
+handle_command_a([["forward", Value]|Tail], SumForward, ODown) :-
 	number_string(NValue, Value),
-	solve_a(Tail, OForward, ODown),
+	handle_command_a(Tail, OForward, ODown),
 	SumForward is NValue + OForward.
-solve_a([["down", Value]|Tail], OForward, SumDown) :-
+handle_command_a([["down", Value]|Tail], OForward, SumDown) :-
 	number_string(NValue, Value),
-	solve_a(Tail, OForward, ODown),
+	handle_command_a(Tail, OForward, ODown),
 	SumDown is NValue + ODown.
-solve_a([["up", Value]|Tail], OForward, SumDown) :-
+handle_command_a([["up", Value]|Tail], OForward, SumDown) :-
 	number_string(NValue, Value),
-	solve_a(Tail, OForward, ODown),
+	handle_command_a(Tail, OForward, ODown),
 	SumDown is ODown - NValue.
+
+
+solve_a(Input, Result) :-
+	handle_command_a(Input, Forward, Down),
+	Result is Forward * Down.
 
 run_a :-
 	read_file_to_string("./input.txt", Input, []),
-	split_string(Input, "\n", "", RowList),
-	maplist(split, RowList, RowList2),
-	solve_a(RowList2, Forward, Down),
-	Result is Forward * Down,
+	format_input(Input, FormattedInput),
+	solve_a(FormattedInput, Result),
 	write(Result).
 
 
+% B
+
+handle_command_b([], _, 0, 0).
+handle_command_b([["forward", Value]|Tail], Aim, SumForward, ResultDown) :-
+	number_string(NValue, Value),
+	handle_command_b(Tail, Aim, OForward, ODown),
+	ResultDown is (Aim * NValue) + ODown,
+	SumForward is NValue + OForward.
+handle_command_b([["down", Value]|Tail], Aim, OForward, ODown) :-
+	number_string(NValue, Value),
+	InAimDown is NValue + Aim,
+	handle_command_b(Tail, InAimDown, OForward, ODown).
+handle_command_b([["up", Value]|Tail], Aim, OForward, ODown) :-
+	number_string(NValue, Value),
+	InAimDown is Aim - NValue,
+	handle_command_b(Tail, InAimDown, OForward, ODown).
+
+
 solve_b(Input, Result) :-
-	Result is Input.
+	handle_command_b(Input, 0, Forward, Down),
+	Result is Forward * Down.
 
 run_b :-
 	read_file_to_string("./input.txt", Input, []),
-	split_string(Input, "\n", "", InputList),
-	to_int(InputList, InputListAsInt),
-	solve_b(InputListAsInt, Result),
+	format_input(Input, FormattedInput),
+	solve_b(FormattedInput, Result),
 	write(Result).
