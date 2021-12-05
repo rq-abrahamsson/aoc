9239aa412f0321aa4757fbe8e64f1283be52aea6
Robin Abrahamsson <robin@abrahamsson.tech>
2021-12-05 22:12:41 +0100

    Done with 2021-3a-prolog

--- 2021-3-prolog/app.pl
+++ 2021-3-prolog/app.pl
@@ -6,6 +6,14 @@ to_int([Y|Tail],[X| Result]) :-
 
 split(In, Out) :- string_chars(In, Out).
 
+range_binary_value(X,X,Y,[Y]) :- !.
+range_binary_value(Low, High, IndexPower, [IndexPower|Xs]) :-
+    Low =< High,
+    NewLow is Low+1,
+    NextIndexPower is IndexPower * 2,
+    range_binary_value(NewLow, High, NextIndexPower, Xs).
+
+
 format_input(Input, FormattedInput) :-
 	split_string(Input, "\n", "", RowList),
 	maplist(split, RowList, FormattedInput).
@@ -19,7 +27,6 @@ get_first([First|_], First).
 get_first_and_rest_lists(T, AllFirsts, AllRests) :-
 	maplist(get_first, T, AllFirsts),
 	maplist(get_rest, T, AllRests).
-
 
 
 count_numbers([], 0, 0).
@@ -66,18 +73,29 @@ get_count_list([Head | Tail], OutOneList, OutZeroList) :-
 	append([OneCount], OutOnes, OutOneList),
 	append([ZeroCount], OutZeroes, OutZeroList).
 
+product(X, Y, Z) :-
+	Z is X * Y.
 
-solve_a(Input, Gamma) :-
+to_decimal(ListNumber, Result) :-
+	reverse(ListNumber, ReversedNumber),
+	length(ReversedNumber, Length),
+	L is Length-1,
+	range_binary_value(0, L, 1, Range),
+	maplist(product, ReversedNumber, Range, Products),
+	foldl(plus, Products, 0, Result).
+
+solve_a(Input, Result) :-
 	new_stuff(Input, OutLists),
 	get_count_list(OutLists, Ones, Zeroes),
 	get_gamma(Ones, Zeroes, Gamma),
 	get_epsilon(Ones, Zeroes, Epsilon),
-	print(Gamma),nl,
-	print(Epsilon),nl.
+	to_decimal(Gamma, GammaDecimal),
+	to_decimal(Epsilon, EpsilonDecimal),
+	Result is GammaDecimal * EpsilonDecimal.
 
 
 run_a :-
-	read_file_to_string("./input-test.txt", Input, []),
+	read_file_to_string("./input.txt", Input, []),
 	format_input(Input, FormattedInput),
 	% write(FormattedInput),nl.
 	solve_a(FormattedInput, Result),
