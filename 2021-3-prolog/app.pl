a351e702279dca21aedf519dfca51fb9c1ecb568
Robin Abrahamsson <robin@abrahamsson.tech>
2021-12-05 16:45:55 +0100

    Start of 2021-3-prolog

---
+++ 2021-3-prolog/app.pl
@@ -0,0 +1,97 @@
+
+to_int([], []).
+to_int([Y|Tail],[X| Result]) :-
+	number_string(X, Y),
+	to_int(Tail, Result).
+
+split(In, Out) :- string_chars(In, Out).
+
+format_input(Input, FormattedInput) :-
+	split_string(Input, "\n", "", RowList),
+	maplist(split, RowList, FormattedInput).
+
+% A
+
+get_rest([_|Rest], Rest).
+
+get_first([First|_], First).
+
+get_first_and_rest_lists(T, AllFirsts, AllRests) :-
+	maplist(get_first, T, AllFirsts),
+	maplist(get_rest, T, AllRests).
+
+
+
+count_numbers([], 0, 0).
+count_numbers([Head | Tail], NewOneCount, ZeroCount) :-
+	Head = '1',
+	count_numbers(Tail, OneCount, ZeroCount),
+	NewOneCount is 1 + OneCount.
+count_numbers([Head | Tail], OneCount, NewZeroCount) :-
+	Head = '0',
+	count_numbers(Tail, OneCount, ZeroCount),
+	NewZeroCount is 1 + ZeroCount.
+
+new_stuff([[]|_], []).
+new_stuff(Input, Result) :-
+	get_first_and_rest_lists(Input, FirstElem, RestElems),
+	new_stuff(RestElems, RestRows),
+	append([FirstElem], RestRows, Result).
+
+gamma(OneCount, ZeroCount, ResultingNumber) :-
+	OneCount > ZeroCount,
+	ResultingNumber is 1.
+gamma(OneCount, ZeroCount, ResultingNumber) :-
+	OneCount < ZeroCount,
+	ResultingNumber is 0.
+
+epsilon(OneCount, ZeroCount, ResultingNumber) :-
+	OneCount < ZeroCount,
+	ResultingNumber is 1.
+epsilon(OneCount, ZeroCount, ResultingNumber) :-
+	OneCount > ZeroCount,
+	ResultingNumber is 0.
+
+
+get_gamma(Ones, Zeroes, Gamma) :-
+	maplist(gamma, Ones, Zeroes, Gamma).
+
+get_epsilon(Ones, Zeroes, Epsilon) :-
+	maplist(epsilon, Ones, Zeroes, Epsilon).
+
+get_count_list([], [], []).
+get_count_list([Head | Tail], OutOneList, OutZeroList) :-
+	count_numbers(Head, OneCount, ZeroCount),
+	get_count_list(Tail, OutOnes, OutZeroes),
+	append([OneCount], OutOnes, OutOneList),
+	append([ZeroCount], OutZeroes, OutZeroList).
+
+
+solve_a(Input, Gamma) :-
+	new_stuff(Input, OutLists),
+	get_count_list(OutLists, Ones, Zeroes),
+	get_gamma(Ones, Zeroes, Gamma),
+	get_epsilon(Ones, Zeroes, Epsilon),
+	print(Gamma),nl,
+	print(Epsilon),nl.
+
+
+run_a :-
+	read_file_to_string("./input-test.txt", Input, []),
+	format_input(Input, FormattedInput),
+	% write(FormattedInput),nl.
+	solve_a(FormattedInput, Result),
+	write(Result).
+
+
+% B
+
+
+solve_b(Input, Result) :-
+	Result is Input.
+
+run_b :-
+	read_file_to_string("./input.txt", Input, []),
+	format_input(Input, FormattedInput),
+	solve_b(FormattedInput, Result),
+	write(Result).
