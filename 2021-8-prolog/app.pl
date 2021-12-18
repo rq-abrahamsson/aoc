fb1b181162eb37819c594b0676d0ad33911d8195
Robin Abrahamsson <robin@abrahamsson.tech>
2021-12-18 11:37:31 +0100

    Prolog 2021-8 2 have solution idea

--- 2021-8-prolog/app.pl
+++ 2021-8-prolog/app.pl
@@ -1,4 +1,5 @@
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
 
 to_int([], []).
 to_int([Y|Tail],[X| Result]) :-
@@ -25,40 +26,17 @@ format_input(Input, Result) :-
 	maplist(get_second_list_element, SplitRows , OutputValues),
 	maplist(format_input_second_pass, OutputValues, Result).
 
-different(X, X) :- !, fail.
-different(_X, _Y).
-
-different(A, A, _, _, _, _, _) :- !, fail.
-different(A, _, A, _, _, _, _) :- !, fail.
-different(A, _, _, A, _, _, _) :- !, fail.
-different(A, _, _, _, A, _, _) :- !, fail.
-different(A, _, _, _, _, A, _) :- !, fail.
-different(A, _, _, _, _, _, A) :- !, fail.
-different(_, B, B, _, _, _, _) :- !, fail.
-different(_, B, _, B, _, _, _) :- !, fail.
-different(_, B, _, _, B, _, _) :- !, fail.
-different(_, B, _, _, _, B, _) :- !, fail.
-different(_, B, _, _, _, _, B) :- !, fail.
-different(_, _, C, C, _, _, _) :- !, fail.
-different(_, _, C, _, C, _, _) :- !, fail.
-different(_, _, C, _, _, C, _) :- !, fail.
-different(_, _, C, _, _, _, C) :- !, fail.
-different(_, _, _, D, D, _, _) :- !, fail.
-different(_, _, _, D, _, D, _) :- !, fail.
-different(_, _, _, D, _, _, D) :- !, fail.
-different(_, _, _, _, E, E, _) :- !, fail.
-different(_, _, _, _, E, _, E) :- !, fail.
-different(_, _, _, _, _, F, F) :- !, fail.
-different(_A, _B, _C, _D, _E, _F, _G).
-
+is_intersect_length(Str1, Str2, Len) :-
+	string_codes(Str1, Str1L),
+	string_codes(Str2, Str2L),
+	intersection(Str1L, Str2L, Intersection),
+	length(Intersection, Length),
+	Length #= Len.
 
 % A
 
-
-
 get_string_length(Input, Output) :-
 	maplist(atom_length, Input, Output).
-
 
 unique_mapper(2, 1).
 unique_mapper(4, 1).
@@ -87,93 +65,135 @@ run_a :-
 
 % B
 
+is_one(_Acc, Str, Str) :-
+	string_length(Str, Len),
+	Len #= 2.
+
+is_one(Acc, _Str, Acc).
+
+get_one(StringList, Result) :-
+	foldl(is_one, StringList, "", Result).
 
 
-% acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
-% cdfeb fcadb cdfeb cdbaf
+is_seven(_Acc, Str, Str) :-
+	string_length(Str, Len),
+	Len #= 3.
 
-% one_is(a, b).
-% four_is(e, a, f, b).
-% seven_is(d, a, b).
-% eight_is(a, c, e, d, g, f, b).
+is_seven(Acc, _Str, Acc).
 
-one(a).
-one(b).
+get_seven(StringList, Result) :-
+	foldl(is_seven, StringList, "", Result).
 
-four(e).
-four(a).
-four(f).
-four(b).
+is_four(_Acc, Str, Str) :-
+	string_length(Str, Len),
+	Len #= 4.
 
-seven(d).
-seven(a).
-seven(b).
+is_four(Acc, _Str, Acc).
 
-eight(a).
-eight(c).
-eight(e).
-eight(d).
-eight(g).
-eight(f).
-eight(b).
+get_four(StringList, Result) :-
+	foldl(is_four, StringList, "", Result).
 
-a_position(X) :-
-	eight(X),
-	seven(X),
-	not(one(X)),
-	not(four(X)).
+is_eight(_Acc, Str, Str) :-
+	string_length(Str, Len),
+	Len #= 7.
 
-b_position(X) :-
-	eight(X),
-	four(X),
-	not(one(X)),
-	not(seven(X)).
+is_eight(Acc, _Str, Acc).
 
-c_position(X) :-
-	eight(X),
-	one(X),
-	four(X),
-	seven(X).
+get_eight(StringList, Result) :-
+	foldl(is_eight, StringList, "", Result).
 
-d_position(X) :-
-	eight(X),
-	four(X),
-	not(seven(X)),
-	not(one(X)).
+is_two(One, Four, Seven, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 1),
+	is_intersect_length(Str, Four, 2),
+	is_intersect_length(Str, Seven, 2).
 
-e_position(X) :-
-	eight(X),
-	not(four(X)),
-	not(seven(X)),
-	not(one(X)).
+is_two(_One, _Four, _Seven, Acc, _Str, Acc).
 
-f_position(X) :-
-	eight(X),
-	four(X),
-	seven(X),
-	one(X).
+get_two(StringList, One, Four, Seven, Result) :-
+	foldl(is_two(One, Four, Seven), StringList, "", Result).
 
-g_position(X) :-
-	eight(X),
-	not(four(X)),
-	not(seven(X)),
-	not(one(X)).
+is_three(One, Two, Four, Seven, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 2),
+	is_intersect_length(Str, Two, 4),
+	is_intersect_length(Str, Four, 3),
+	is_intersect_length(Str, Seven, 3).
+	
 
+is_three(_One, _Two, _Four, _Seven, Acc, _Str, Acc).
 
-numbers(A, B, C, D, E, F, G) :-
-	a_position(A),
-	b_position(B),
-	c_position(C),
-	d_position(D),
-	e_position(E),
-	f_position(F),
-	g_position(G),
-	different(A, B, C, D, E, F, G).
+get_three(StringList, One, Two, Four, Seven, Result) :-
+	foldl(is_three(One, Two, Four, Seven), StringList, "", Result).
+
+is_five(One, Two, Three, Four, Seven, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 1),
+	is_intersect_length(Str, Two, 3),
+	is_intersect_length(Str, Three, 4),
+	is_intersect_length(Str, Four, 3),
+	is_intersect_length(Str, Seven, 2).
+	
+
+is_five(_One, _Two, _Three, _Four, _Seven, Acc, _Str, Acc).
+
+get_five(StringList, One, Two, Three, Four, Seven, Result) :-
+	foldl(is_five(One, Two, Three, Four, Seven), StringList, "", Result).
+
+is_six(One, Two, Three, Four, Five, Seven, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 1),
+	is_intersect_length(Str, Two, 4),
+	is_intersect_length(Str, Three, 4),
+	is_intersect_length(Str, Four, 3),
+	is_intersect_length(Str, Five, 5),
+	is_intersect_length(Str, Seven, 2).
+	
+
+is_six(_One, _Two, _Three, _Four, _Five, _Seven, Acc, _Str, Acc).
+
+get_six(StringList, One, Two, Three, Four, Five, Seven, Result) :-
+	foldl(is_six(One, Two, Three, Four, Five, Seven), StringList, "", Result).
 
 
+is_nine(One, Two, Three, Four, Five, Six, Seven, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 2),
+	is_intersect_length(Str, Two, 4),
+	is_intersect_length(Str, Three, 5),
+	is_intersect_length(Str, Four, 4),
+	is_intersect_length(Str, Five, 5),
+	is_intersect_length(Str, Six, 5),
+	is_intersect_length(Str, Seven, 3).
+	
 
+is_nine(_One, _Two, _Three, _Four, _Five, _Six, _Seven, Acc, _Str, Acc).
 
+get_nine(StringList, One, Two, Three, Four, Five, Six, Seven, Result) :-
+	foldl(is_nine(One, Two, Three, Four, Five, Six, Seven), StringList, "", Result).
 
+is_zero(One, Two, Three, Four, Five, Six, Seven, Nine, _Acc, Str, Str) :-
+	is_intersect_length(Str, One, 2),
+	is_intersect_length(Str, Two, 4),
+	is_intersect_length(Str, Three, 4),
+	is_intersect_length(Str, Four, 3),
+	is_intersect_length(Str, Five, 4),
+	is_intersect_length(Str, Six, 5),
+	is_intersect_length(Str, Seven, 3),
+	is_intersect_length(Str, Nine, 5).
+	
+
+is_zero(_One, _Two, _Three, _Four, _Five, _Six, _Seven, _Nine, Acc, _Str, Acc).
+
+get_zero(StringList, One, Two, Three, Four, Five, Six, Seven, Nine, Result) :-
+	foldl(is_zero(One, Two, Three, Four, Five, Six, Seven, Nine), StringList, "", Result).
+
+numbers(Input, [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]) :-
+	get_one(Input, One),
+	get_four(Input, Four),
+	get_seven(Input, Seven),
+	get_eight(Input, Eight),
+	get_two(Input, One, Four, Seven, Two),
+	get_three(Input, One, Two, Four, Seven, Three),
+	get_five(Input, One, Two, Three, Four, Seven, Five),
+	get_six(Input, One, Two, Three, Four, Five, Seven, Six),
+	get_nine(Input, One, Two, Three, Four, Five, Six, Seven, Nine),
+	get_zero(Input, One, Two, Three, Four, Five, Six, Seven, Nine, Zero).
 
 solve_b(Result, Result).
 
